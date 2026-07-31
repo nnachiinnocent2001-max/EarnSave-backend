74001b5ee58e0b6917e82b32af2ddf53b669a3a6
nnachiinnocent2001-max <nnachiinnocent2001@gmail.com>
2026-07-31 09:09:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,18 +13,16 @@ app.use(cors());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api', require('./routes/main'));
 
-// Health check route
+// Health check
 app.get('/', (req, res) => {
   res.json({ message: "EarnSave API is running" });
 });
 
 // Connect to MongoDB
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI)
-  .then(() => {
+mongoose.connect(process.env.MONGO_URI)
+ .then(() => {
     console.log('MongoDB Connected ✅');
     app.listen(PORT, () => console.log(`Server Running on ${PORT}`));
   })
-  .catch(err => console.log('Mongo Error:', err));
+ .catch(err => console.log('Mongo Error:', err));
